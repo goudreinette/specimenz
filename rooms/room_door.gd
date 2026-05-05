479ace8b71f0df6ab3bad65e7dc63a37752c0239
Rein van der Woerd <reinvanderwoerd@me.com>
2026-05-05 22:04:56 +0200

    add menu, hallway, living room, lab (almost)

--- rooms/room_door.gd
+++ rooms/room_door.gd
@@ -1,15 +1,17 @@
 class_name RoomDoor extends TextureButton
 
 
-@export var next_scene: PackedScene
+@export var next_scene: String
 
 
 func _on_pressed():
 	print("pressed!")
-	$AnimationPlayer.play("fadeout")
+	#get_tree().change_scene_to_packed(next_scene)
+	get_tree().change_scene_to_file(next_scene)
+	#$AnimationPlayer.play("fadeout")
 	
 
 
-func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "fadeout":
-		get_tree().change_scene_to_packed(next_scene)
+#func _on_animation_player_animation_finished(anim_name):
+	#if anim_name == "fadeout":
+		#get_tree().change_scene_to_packed(next_scene)
