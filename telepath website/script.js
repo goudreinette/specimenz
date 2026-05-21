9135fd928d210f9ecdf69fbff9627907819f6b34
Rein van der Woerd <reinvanderwoerd@me.com>
2026-05-21 19:21:12 +0200

    alternative version with lines instead of polylines

--- telepath website/script.js
+++ telepath website/script.js
@@ -34,7 +34,9 @@ function addPolyline(startX, startY) {
         currentX: startX,
         currentY: startY,
         points: "",
+        svgLines: "",
         active: true,
+        i: 0,
         stepsSinceTouchedLetter: 0
     })
 }
@@ -52,14 +54,18 @@ setInterval(() => {
 
     for (const line of polylines) {
         if (line.active) {
+            line.i += 1
             line.stepsSinceTouchedLetter += 1 
 
             line.prevX = line.currentX
-            line.prevX = line.currentX
+            line.prevY = line.currentY
             line.currentY = line.currentY + randomRange(-stepSize, stepSize)
             line.currentX = line.currentX + randomRange(-stepSize, stepSize)
             
-            line.points += `${line.currentX},${line.currentY} `
+            // line.points += `${line.currentX},${line.currentY} `
+
+            const color = colorMixer([0,255,0], [255,0,255], Math.abs(Math.sin(line.i / 1000)))
+            line.svgLines += `<line x1="${line.prevX}" y1="${line.prevY}" x2="${line.currentX}" y2="${line.currentY}" stroke="${color}" />`
 
             const divideChance = line.stepsSinceTouchedLetter < 100 ? .99 : .9997
        
@@ -82,9 +88,11 @@ setInterval(() => {
 
         }
 
-        newSvgContents += `
-            <polyline points="${line.points}" fill="none" stroke="olive" />
-        `
+        // newSvgContents += `
+        //     <polyline points="${line.points}" fill="none" stroke="olive" />
+        // `
+
+        newSvgContents += line.svgLines
         
         // Chance of dying
         if (Math.random() > .999 || line.stepsSinceTouchedLetter > 1000) {
@@ -116,4 +124,20 @@ addPolyline(firstCharRect.x,firstCharRect.y)
 
 function randomRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+//colorChannelA and colorChannelB are ints ranging from 0 to 255
+function colorChannelMixer(colorChannelA, colorChannelB, amountToMix){
+    var channelA = colorChannelA*amountToMix;
+    var channelB = colorChannelB*(1-amountToMix);
+    return parseInt(channelA+channelB);
+}
+//rgbA and rgbB are arrays, amountToMix ranges from 0.0 to 1.0
+//example (red): rgbA = [255,0,0]
+function colorMixer(rgbA, rgbB, amountToMix){
+    var r = colorChannelMixer(rgbA[0],rgbB[0],amountToMix);
+    var g = colorChannelMixer(rgbA[1],rgbB[1],amountToMix);
+    var b = colorChannelMixer(rgbA[2],rgbB[2],amountToMix);
+    return "rgb("+r+","+g+","+b+")";
 }
