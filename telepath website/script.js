da018043ee961198be31ec0f440521d42376a7e2
Rein van der Woerd <reinvanderwoerd@me.com>
2026-05-21 17:54:01 +0200

    physarum svg

--- telepath website/script.js
+++ telepath website/script.js
@@ -1,15 +1,98 @@
 // Split into spans
+let chars = null
 Splitting().forEach(s => {
-    s.chars.forEach(char => {
+    chars = s.chars
 
-        console.log(char)
-        setTimeout(() => {
-            const duration = 10 + (Math.random() * 20)
-            char.classList.add('fading')
-            char.style.setProperty("--duration", `${duration}s`);
-        }, Math.random() * 30 * 1000)
-        // if (word.parentElement.getAttribute('aria-label')) {
-        //     word.setAttribute('aria-hidden', true)
-        // }
+    s.chars.forEach(char => {
+        // setTimeout(() => {
+        //     startDecaying(char)
+        // }, Math.random() * 30 * 1000)
     })
 })
+
+
+function startDecaying(char) {
+    console.log("Start decaying ", char)
+    const duration = (Math.random() * 10)
+    char.classList.add('fading')
+    char.style.setProperty("--duration", `${duration}s`);
+} 
+
+
+// Physarum SVG
+const physarumSvg = document.querySelector('svg#physarum')
+let polylines = []
+const stepSize = 20
+
+function addPolyline(startX, startY) {
+    polylines.push({
+        prevX: startX,
+        prevY: startY, 
+        currentX: startX,
+        currentY: startY,
+        points: "",
+        active: true
+    })
+}
+
+
+// Animate line
+setInterval(() => {
+    let newSvgContents = ""
+    for (const line of polylines) {
+        if (line.active) {
+            line.prevX = line.currentX
+            line.prevX = line.currentX
+            line.currentY = line.currentY + randomRange(-stepSize, stepSize)
+            line.currentX = line.currentX + randomRange(-stepSize, stepSize)
+            
+            line.points += `${line.currentX},${line.currentY} `
+
+            if (Math.random() > .995 && polylines.length < 10) {
+                addPolyline(line.currentX, line.currentY)
+            }
+
+            for (const char of chars) {
+                const rect = char.getBoundingClientRect()
+                var distFromPoint = Math.abs(line.currentX - rect.x) + Math.abs(line.currentY - (rect.y));
+                // var b = line.currentY - rect.top;
+                // const distFromPoint = Math.sqrt( a*a + b*b );
+                if (distFromPoint < 500) {
+                    console.log(char, ' close')
+                    startDecaying(char)
+                }
+            }
+        }
+
+        newSvgContents += `
+            <polyline points="${line.points}" fill="none" stroke="olive" />
+        `
+        
+        // Chance of dying
+        if (Math.random() > .999) {
+            let activeLinesCount = 0
+            for (const l of polylines) {
+                if (l.active) {
+                    activeLinesCount++
+                }
+            }
+
+            // Don't be the only leftover active line
+            if (activeLinesCount > 1) {
+                line.active = false
+            }
+        }
+    }
+
+    physarumSvg.innerHTML = newSvgContents
+}, 50)
+
+
+
+// Initial point
+addPolyline(300,200)
+
+
+function randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
