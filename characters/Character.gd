62ef369d99c21274d78c5ee196a414d399c64093
Rein van der Woerd <reinvanderwoerd@me.com>
2026-05-31 16:53:36 +0200

    text bubbles and talking animation

--- characters/Character.gd
+++ characters/Character.gd
@@ -1,11 +1,14 @@
-extends Node2D
+class_name Character extends Node2D
+
+
+@export var text_bubble : PackedScene
 
 
 var start_pos : Vector2 
-
-@export var text_lines : Array[String]
 @export var last_frame_pos : Vector2 = self.position
 
+var talking: bool
+var current_walking_tween: Tween
 
 func _ready():
 	start_pos = self.get_position()
@@ -18,35 +21,71 @@ func _process(delta):
 	scale = Vector2(s, s)
 	
 	# Set animation frames!
-	var going_left = self.position.x - last_frame_pos.x < 0
-	var going_up = self.position.y - last_frame_pos.y < 0
+	var left_right_diff = self.position.x - last_frame_pos.x
+	var up_down_diff = self.position.y - last_frame_pos.y
+	var going_left = left_right_diff < 0
+	var going_up = up_down_diff < 0
 	var distance: float = self.position.distance_to(last_frame_pos)
 	
-	if distance > .05:
-		if going_left:
-			if going_up:
-				$AnimatedSprite2D.play("left_backward")
-			else:
-				$AnimatedSprite2D.play("left_forward")
-		else:
-			if going_up:
-				$AnimatedSprite2D.play("right_backward")
-			else:
-				$AnimatedSprite2D.play("right_forward")
-	else: 
-		$AnimatedSprite2D.play("default")
-			
 	
-	
-		
+	if talking:
+		$AnimatedSprite2D.play("talking")
+	else:
+		# Walking
+		if distance > 0.0:
+			if going_left:
+				if going_up:
+					$AnimatedSprite2D.play("left_backward")
+				else:
+					$AnimatedSprite2D.play("left_forward")
+			else:
+				if going_up:
+					$AnimatedSprite2D.play("right_backward")
+				else:
+					$AnimatedSprite2D.play("right_forward")
+		# Idling
+		else: 
+			$AnimatedSprite2D.play("idling")
+
 	last_frame_pos = self.position
 
 
 func _on_timer_timeout():
-	var tween = get_tree().create_tween()
-	var new_pos = Vector2(randf_range(-30, 30), randf_range(-20, 100))
-	tween.tween_property(self, "position", start_pos + new_pos, 2.0)
-	#tween.tween_property($Sprite, "scale", Vector2(), 1.0)
-	print(self.name,  "'s TIMER WENT OFF, moving to ", new_pos)
-	$Timer.wait_time = randf_range(1, 7)
-	$Timer.start()
+	if not talking: 
+		current_walking_tween =  get_tree().create_tween()
+		var new_pos =  start_pos +  Vector2(randf_range(-30, 30), randf_range(20, 80))
+		#var distance = self.position.distance_to(new_pos)
+		#var walk_duration = remap(distance, 0, 200, 0.0, 1.0)
+		const SPEED = 30
+		var distance = (position).distance_to(new_pos)
+		var tween_time = distance / SPEED
+		print(position, new_pos)
+		current_walking_tween.tween_property(self, "position", new_pos, tween_time)
+		#tween.tween_property($Sprite, "scale", Vector2(), 1.0)
+		var next_duration = randf_range(5, 10)
+		print(self.name,  "'s TIMER WENT OFF, moving to ", new_pos)
+		print(self.name,  "'s next timer in ", next_duration)
+		$Timer.start(next_duration)
+
+func _on_talking_timer_timeout():
+	talking = false
+	current_walking_tween = null
+	_on_timer_timeout()	
+
+func say(text: String):
+	print("saying: ", text)
+	var new_bubble: SpeechBubble = text_bubble.instantiate()
+	new_bubble.position = position
+	new_bubble.set_text(text)
+	if current_walking_tween:
+		current_walking_tween.kill()
+	$TalkingTimer.start(3)
+	talking = true
+	
+	get_parent().add_child(new_bubble)
+
+
+
+func sigh():
+	pass
+	
